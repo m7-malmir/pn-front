d9d61a3d82d73da77f8685f0345872721ea3d413
Mohammad Malmir <m.malmir666@gmail.com>
2024-01-25 02:08:59 +0330

    autoload added to project

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-require_once './controllers/Dbh.classes.php';
+spl_autoload_register('autoload');
+function autoload($classname){
+    include dirname(__FILE__).'/'.str_replace('\\','/',$classname).'.php';   
+}
+
+$ok=new \controllers\Dbh;
+
+print_r($ok);
