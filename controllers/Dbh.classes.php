9bfdd170ba16264d93836df441d9d32237c4c57b
Mohammad Malmir <m.malmir666@gmail.com>
2024-01-25 01:34:37 +0330

    connect to database

---
+++ controllers/Dbh.classes.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+define("ROOT_URL",'http://localhost/pnikpay/');
+class Dbh{
+  public function connect(){
+        try {
+            $username="root";
+            $password="";
+            $dbh=new PDO("mysql:host=localhost;dbname=codexworld",$username,$password);
+            return $dbh;
+        } catch (\Throwable $th) {
+            print 'Error!: '. $th->getMessage() ."<br/>";
+            die();
+        }
+  }
+}
+
