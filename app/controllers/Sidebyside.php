71940d5894accedbbcc2fa10411d197b82917a1b
m7-malmir <m.malmir666@gmail.com>
2024-03-02 23:58:21 +0330

    file side added

---
+++ app/controllers/Sidebyside.php
@@ -0,0 +1,19 @@
+<?php
+
+class Sidebyside
+{
+    use Controller;
+    public function index()
+    {
+
+    $data['username']=empty($_SESSION['USER']) ? 'user':  $_SESSION['USER']->users_uid;
+    $data['admin']=empty($_SESSION['admin']) ? '':  $_SESSION['admin'];
+     $this->view('sidebyside',$data);
+
+    }
+    // public function edit($a = '', $b = '', $c = '')
+    // {
+    //     show('from the edit function');
+    //     $this->view('home');
+    // }
+}
